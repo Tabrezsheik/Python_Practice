9e89c2561c9a29da9c8c925ce69afd2f477879fd
Tabrezsheik <133689080+Tabrezsheik@users.noreply.github.com>
2026-06-16 20:02:30 +0530

    Delete Variables,conditionals directory

--- Variables,conditionals/elif.py
+++
@@ -1,7 +0,0 @@
-num = int(input("enter a Value: "))
-if (num) >0:
-        print(f"Is an positive number : {num}")
-elif (num) <0:
-        print(f"Is an Negative number : {num}")
-else:
-        print(f"Imaginary Numbers")
